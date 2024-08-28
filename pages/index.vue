107e35196445c7e8c9a11b43eadae310e5470102
ATM-Shifat <shifatatm@gmail.com>
2024-08-28 17:48:58 +0600

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    Hello from index
+    <Button @click="click">Click me</Button>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const click = () => {
+  alert("Click")
+}
+</script>
+
+<style>
+
+</style>
